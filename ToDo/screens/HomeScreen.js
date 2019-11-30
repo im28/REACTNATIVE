644a57c2f579ac567332674647f1deb584d3f597
im28 <himaboy.im@gmail.com>
2019-11-30 22:48:30 +0800

    Components

--- ToDo/screens/HomeScreen.js
+++ ToDo/screens/HomeScreen.js
@@ -11,13 +11,16 @@ import {
 import { LinearGradient } from 'expo-linear-gradient';
 import uuid from 'uuid/v1';
 import { primaryGradientArray } from '../components/utils/Colors';
-
-import { MonoText } from '../components/StyledText';
+import Header from './components/Header';
+import SubTitle from './components/SubTitle';
+import Input from './components/Input';
+import List from './components/List';
+import Button from './components/Button';
 
 export default function HomeScreen() {
   return (
     <LinearGradient colors={primaryGradientArray} style={styles.container}>
-
+        <StatusBar barStyle="light-content" />
     </LinearGradient>
   );
 }
