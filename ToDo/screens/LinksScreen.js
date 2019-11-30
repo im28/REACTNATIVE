f8b6fb5cdbba339d46c8171cc2b14db6fd96505d
im28 <himaboy.im@gmail.com>
2019-11-30 23:41:16 +0800

    scanner Done

--- ToDo/screens/LinksScreen.js
+++ ToDo/screens/LinksScreen.js
@@ -1,16 +1,47 @@
-import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import React,{useState,useEffect} from 'react';
+import { Text, View, StyleSheet, Button,ScrollView } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
 
 export default function LinksScreen() {
+  const [hasCameraPermission, sethasCameraPermission] = useState(null)
+  const [scanned, setscanned] = useState(false)
+  useEffect(() => {
+    getPermissionsAsync();
+  }, [])
+
+    
+  async function getPermissionsAsync (){
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    sethasCameraPermission(  status === 'granted' );
+  };
+  handleBarCodeScanned = ({ type, data }) => {
+    setscanned( true );
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+  if (hasCameraPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasCameraPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
-    <ScrollView style={styles.container}>
-      {/**
-       * Go ahead and delete ExpoLinksView and replace it with your content;
-       * we just wanted to provide you with some helpful links.
-       */}
-      <ExpoLinksView />
-    </ScrollView>
+    <View
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+          justifyContent: 'flex-end',
+        }}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+
+        {scanned && (
+          <Button title={'Tap to Scan Again'} onPress={() => setscanned(false)} />
+        )}
+      </View>
   );
 }
 
